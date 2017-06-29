0453a7caa48eb614ceff2ee91624a5573f8c39fd
Michael T Smith <lionfishy@gmail.com>
2017-06-29 12:21:40 +0100

    module dependencies in setup

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'dp4gp',
   packages = ['dp4gp'], # this must be the same as the name above
-  version = '1.0',
+  version = '1.01',
   description = 'Differential Privacy for Gaussian Processes',
   author = 'Mike Smith',
   author_email = 'm.t.smith@sheffield.ac.uk',
-  url = 'https://github.com/lionfish0/dp4gp.git', # use the URL to the github repo
-  download_url = 'https://github.com/lionfish0/dp4gp/archive/1.0.tar.gz', # I'll explain this in a second
-  keywords = ['differential privacy','gaussian processes'], # arbitrary keywords
+  url = 'https://github.com/lionfish0/dp4gp.git',
+  download_url = 'https://github.com/lionfish0/dp4gp/archive/1.01.tar.gz',
+  keywords = ['differential privacy','gaussian processes'],
   classifiers = [],
+  install_requires=['GPy','numpy','sklearn','scipy'],
 )
