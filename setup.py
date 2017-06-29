7f3e6791cfa5ded54779d6db401e658e937a9ccc
Michael T Smith <lionfishy@gmail.com>
2017-06-29 11:50:01 +0100

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dp4gp',
+  packages = ['dp4gp'], # this must be the same as the name above
+  version = '0.9',
+  description = 'Differential Privacy for Gaussian Processes',
+  author = 'Mike Smith',
+  author_email = 'm.t.smith@sheffield.ac.uk',
+  url = 'https://github.com/lionfish0/dp4gp.git', # use the URL to the github repo
+  download_url = 'https://github.com/lionfish0/dp4gp/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['differential privacy','gaussian processes'], # arbitrary keywords
+  classifiers = [],
+)
